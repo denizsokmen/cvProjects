10378edb818bd29fe497209c03dc59b5607625a3
denizsokmen <xantier277@gmail.com>
2014-11-22 08:01:14 +0200

    backup

--- hw4/PRJ4-S003244.cpp
+++ hw4/PRJ4-S003244.cpp
@@ -8,61 +8,62 @@ using namespace std;
 
 void help()
 {
- cout << "\nThis program demonstrates line finding with the Hough transform.\n"
-         "Usage:\n"
-         "./houghlines <image_name>, Default is pic1.jpg\n" << endl;
+    cout << "\nThis program demonstrates line finding with the Hough transform.\n"
+	"Usage:\n"
+	"./houghlines <image_name>, Default is pic1.jpg\n" << endl;
 }
 
 int main(int argc, char** argv)
 {
 
- const char* filename = argc >= 2 ? argv[1] : "pic1.jpg";
+    const char* filename = argc >= 2 ? argv[1] : "pic1.jpg";
 
- Mat src = imread(filename, 0);
+    Mat src = imread(filename, 0);
 
- if(src.empty())
- {
-     help();
+    if(src.empty())
+    {
+	help();
 
-     cout << "can not open " << filename << endl;
-     return -1;
- }
+	cout << "can not open " << filename << endl;
+	return -1;
+    }
 
- Mat dst, cdst;
- //Canny(src, dst, 50, 200, 3);
- cv::threshold(src, dst, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
- cvtColor(dst, cdst, CV_GRAY2BGR);
+    Mat dst, cdst;
+    bitwise_not(src, src);
+    //Canny(src, dst, 50, 200, 3);
+    cv::threshold(src, dst, 0, 255, CV_THRESH_TOZERO | CV_THRESH_OTSU);
+    cvtColor(dst, cdst, CV_GRAY2BGR);
 
- #if 0
-  vector<Vec2f> lines;
-  HoughLines(dst, lines, 1, CV_PI/180, 100, 0, 0 );
+#if 0
+    vector<Vec2f> lines;
+    HoughLines(dst, lines, 1, CV_PI/180, 100, 0, 0 );
 
-  for( size_t i = 0; i < lines.size(); i++ )
-  {
-     float rho = lines[i][0], theta = lines[i][1];
-     Point pt1, pt2;
-     double a = cos(theta), b = sin(theta);
-     double x0 = a*rho, y0 = b*rho;
-     pt1.x = cvRound(x0 + 1000*(-b));
-     pt1.y = cvRound(y0 + 1000*(a));
-     pt2.x = cvRound(x0 - 1000*(-b));
-     pt2.y = cvRound(y0 - 1000*(a));
-     line( cdst, pt1, pt2, Scalar(0,0,255), 3, CV_AA);
-  }
- #else
-  vector<Vec4i> lines;
-  HoughLinesP(dst, lines, 1, CV_PI/180, 50, 50, 10 );
-  for( size_t i = 0; i < lines.size(); i++ )
-  {
-    Vec4i l = lines[i];
-    line( cdst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, CV_AA);
-  }
- #endif
- imshow("source", src);
- imshow("detected lines", cdst);
+    for( size_t i = 0; i < lines.size(); i++ )
+    {
+    float rho = lines[i][0], theta = lines[i][1];
+    Point pt1, pt2;
+    double a = cos(theta), b = sin(theta);
+    double x0 = a*rho, y0 = b*rho;
+    pt1.x = cvRound(x0 + 1000*(-b));
+    pt1.y = cvRound(y0 + 1000*(a));
+    pt2.x = cvRound(x0 - 1000*(-b));
+    pt2.y = cvRound(y0 - 1000*(a));
+    line( cdst, pt1, pt2, Scalar(0,0,255), 3, CV_AA);
+}
+#else
+    vector<Vec4i> lines;
+    HoughLinesP(dst, lines, 1, CV_PI/180, 50, 50, 10 );
+    for( size_t i = 0; i < lines.size(); i++ )
+    {
+	Vec4i l = lines[i];
+	line( cdst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, CV_AA);
+    }
+#endif
+    imshow("source", src);
+    imshow("detected lines", cdst);
 
- waitKey();
+    waitKey();
 
- return 0;
+    return 0;
 }
 
